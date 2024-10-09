fcb5af80cb80e9777a73e5e2ea4626c3086833e7
ABEC-projects <70073993+ABEC-projects@users.noreply.github.com>
2024-10-09 09:28:04 +0300

    A bit more beauty

--- options.nix
+++ options.nix
@@ -3,6 +3,13 @@ let
 nixgl = import <nixgl>{};
 package-path = nixgl.auto.nixGLDefault or "";
 exec = if package-path != "" then "${package-path}/bin/nixGL" else "";
+mkOptionDisabled = description:
+  lib.mkOption {
+    type = with lib.types; uniq bool;
+    default = false;
+    example = true;
+    inherit description;
+  };
 in
 {
   options.nixGLPrefix = lib.mkOption {
@@ -14,20 +21,14 @@ in
       This needs to be set to the right nixGL package on non-NixOS systems.
     '';
   };
-  options.useWayland = lib.mkOption {
-    type = with lib.types; uniq bool;
-    default = false;
-    example = true;
-    description = ''
-      Weather to include wayland-specific configs, packages, etc.
-    '';
-  };
-  options.useX11 = lib.mkOption {
-    type = with lib.types; uniq bool;
-    default = false;
-    example = true;
-    description = ''
-      Weather to include wayland-specific configs, packages, etc.
-    '';
-  };
+  options.useWayland = mkOptionDisabled ''
+    Weather to include wayland-specific configs, packages, etc.
+  '';
+  options.useX11 = mkOptionDisabled ''
+    Weather to include X11-specific configs, packages, etc.
+  '';
+  options.useNixGL = mkOptionDisabled ''
+    Weather to include nixGL overlays.
+    May be helpful on not nixOS distros if you are having problems with some apps
+  '';
 }
